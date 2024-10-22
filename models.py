bd96d2bcff5f3d73198121a3888443381989995d
Hashara13 <hnethmin13@gmail.com>
2024-10-22 11:20:01 +0530

    amend models

--- models.py
+++ models.py
@@ -18,10 +18,38 @@ class AirQualityIndex(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(200), nullable=False)
     aqi = db.Column(db.Float, nullable=False)
+    pm25 = db.Column(db.Float, nullable=False)
+    o3 = db.Column(db.Float, nullable=False)
+    co = db.Column(db.Float, nullable=False)
+    no2 = db.Column(db.Float, nullable=False)
+    so2 = db.Column(db.Float, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 class TrafficDensity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(200), nullable=False)
     density = db.Column(db.Float, nullable=False)
+    vehicle_count = db.Column(db.Integer, nullable=False)
+    average_speed = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class NoiseLevel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(200), nullable=False)
+    decibel = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class WaterQuality(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(200), nullable=False)
+    ph = db.Column(db.Float, nullable=False)
+    turbidity = db.Column(db.Float, nullable=False)
+    dissolved_oxygen = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+class EnergyConsumption(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(200), nullable=False)
+    consumption = db.Column(db.Float, nullable=False)  # in kWh
+    renewable_percentage = db.Column(db.Float, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
